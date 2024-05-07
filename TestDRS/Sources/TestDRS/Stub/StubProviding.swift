9b7b21218d22a4c2fcdda03b61063e4e95c30415
Shawn Gee <sgee@turo.com>
2024-05-07 15:15:08 -0700

    Add core stub functionality and tests (#3)
    * feat: add core stub functionality
    
    * feat: add support for stubbing async functions
    
    * chore: fix warnings
    
    * chore: update protocol documentation example

---
+++ TestDRS/Sources/TestDRS/Stub/StubProviding.swift
@@ -0,0 +1,143 @@
+//
+// Created on 5/1/24.
+// Copyright © 2024 Turo Open Source. All rights reserved.
+//
+
+import Foundation
+
+/// `StubProviding` is a protocol that defines a stubbing mechanism for unit testing.
+/// It allows you to replace real method calls with stubbed responses, making it easier to test your code in isolation.
+/// Each stub is associated with the function's signature, input type, and output type.
+/// This information is used to retrieve the stub when the function is called.
+///
+/// Example usage:
+/// ```
+/// class MyClass: StubProviding {
+///     let stubRegistry = StubRegistry()
+///
+///     func foo() -> Int {
+///         stubOutput()
+///     }
+/// }
+///
+/// let myClass = MyClass()
+/// myClass.setStub(for: myClass.foo, withSignature: "foo()", returning: 42)
+/// print(myClass.foo())  // Prints "42"
+/// ```
+public protocol StubProviding {
+    var stubRegistry: StubRegistry { get }
+}
+
+public extension StubProviding {
+
+    // MARK: - Set
+
+    /// Sets a stub for a given function to return a provided output.
+    ///
+    /// - Parameters:
+    ///   - function: The function to stub.
+    ///   - signature: The signature of the function to stub, which can be obtained by right-clicking on the function's signature and selecting "Copy" > "Copy Symbol Name".
+    ///   This should also match what is recorded by the `#function` macro.
+    ///   - output: The output value to be returned when the function is called.
+    ///
+    ///   - Note: The compiler will not be able to disambiguate when stubbing functions that are overloaded *and* share the same output type.
+    ///   If that is the case, use `setDynamicStub(for:withSignature:using:)` and specify the input to the closure explicitly.
+    func setStub<Input, Output>(
+        for function: (Input) async throws -> Output,
+        withSignature signature: String,
+        returning output: Output
+    ) {
+        stubRegistry.register(output: output, for: function, withSignature: signature)
+    }
+
+    /// Sets a stub for a given function to throw a provided error.
+    ///
+    /// - Parameters:
+    ///   - function: The function to stub.
+    ///   - signature: The signature of the function to stub, which can be obtained by right-clicking on the function's signature and selecting "Copy" > "Copy Symbol Name".
+    ///   This should also match what is recorded by the `#function` macro.
+    ///   - error: The error to be thrown when the function is called.
+    ///
+    ///   - Note: The compiler will not be able to disambiguate when stubbing functions that are overloaded *and* share the same output type.
+    ///   If that is the case, use `setDynamicStub(for:withSignature:using:)` and specify the input to the closure explicitly.
+    func setStub<Input, Output>(
+        for function: (Input) async throws -> Output,
+        withSignature signature: String,
+        throwing error: Error
+    ) {
+        stubRegistry.register(error: error, for: function, withSignature: signature)
+    }
+
+    /// Sets a stub for a given function using a closure to dynamically determine the output.
+    ///
+    /// - Parameters:
+    ///   - function: The function to stub.
+    ///   - signature: The signature of the function to stub, which can be obtained by right-clicking on the function's signature and selecting "Copy" > "Copy Symbol Name".
+    ///   This should also match what is recorded by the `#function` macro.
+    ///   - closure: A closure that takes in the function's input and returns the desired output when the function is called.
+    func setDynamicStub<Input, Output>(
+        for function: (Input) async throws -> Output,
+        withSignature signature: String,
+        using closure: @escaping (Input) throws -> Output
+    ) {
+        stubRegistry.register(closure: closure, forSignature: signature)
+    }
+
+    // MARK: - Get
+
+    /// Retrieves the stubbed output for the calling function based on the given input and expected output type.
+    ///
+    /// - Parameters:
+    ///   - input: The input to the calling function.
+    ///   - signature: **Do not pass in this argument**, it will automatically capture the signature of the calling function.
+    /// - Returns: The stubbed output for the calling function.
+    ///
+    /// - Precondition: A corresponding stub must be set prior to calling this function. Otherwise, a fatal error will be thrown.
+    func stubOutput<Input, Output>(
+        for input: Input = Void(),
+        signature: String = #function
+    ) -> Output {
+        do {
+            return try stubRegistry.getOutput(for: input, withSignature: signature)
+        } catch {
+            if let stubError = error as? StubRegistry.StubError {
+                report(stubError, signature: signature, input: Input.self, output: Output.self)
+            }
+            fatalError("Unexpected error getting stub for \(signature)")
+        }
+    }
+
+    /// Retrieves the stubbed output for the calling function based on the given input and expected output type, allowing for potential throwing of errors.
+    ///
+    /// - Parameters:
+    ///   - input: The input to the calling function.
+    ///   - signature: **Do not pass in this argument**, it will automatically capture the signature of the calling function.
+    /// - Returns: The stubbed output for the calling function, provided one has been set.
+    /// - Throws: Any error that has been set to be thrown for this function.
+    func throwingStubOutput<Input, Output>(
+        for input: Input = Void(),
+        signature: String = #function
+    ) throws -> Output {
+        do {
+            return try stubRegistry.getOutput(for: input, withSignature: signature)
+        } catch let stubError as StubRegistry.StubError {
+            report(stubError, signature: signature, input: Input.self, output: Output.self)
+            throw stubError
+        }
+    }
+
+    private func report<Input, Output>(
+        _ stubError: StubRegistry.StubError,
+        signature: String,
+        input: Input.Type,
+        output: Output.Type
+    ) {
+        switch stubError {
+        case .noStub:
+            fatalError("No stub with input `\(Input.self)` and output `\(Output.self)` was set for \(signature).")
+        case .incorrectOutputType, .incorrectClosureType:
+            fatalError("This should not happen, there must be an issue in TestDRS within the `StubProviding` protocol and/or the `StubRegistry`.")
+        }
+    }
+
+}
---
+++ TestDRS/Tests/TestDRSTests/Stub/StubProvidingTests.swift
@@ -0,0 +1,226 @@
+//
+// Created on 5/1/24.
+// Copyright © 2024 Turo Open Source. All rights reserved.
+//
+
+@testable import TestDRS
+import XCTest
+
+final class StubProvidingTests: XCTestCase {
+
+    private let stubProvider = StubProvider()
+
+    func testStubbingFunctionWithNoParameters() {
+        stubProvider.setStub(for: stubProvider.foo, withSignature: "foo()", returning: 7)
+
+        XCTAssertEqual(stubProvider.foo(), 7)
+    }
+
+    func testDynamicallyStubbingFunctionWithNoParameters() {
+        var x = 1
+        stubProvider.setDynamicStub(for: stubProvider.foo, withSignature: "foo()") {
+            x * 7
+        }
+
+        XCTAssertEqual(stubProvider.foo(), 7)
+        x = 7
+        XCTAssertEqual(stubProvider.foo(), 49)
+    }
+
+    func testStubbingFunctionWithSingleParameter() {
+        stubProvider.setStub(for: stubProvider.bar(paramOne:), withSignature: "bar(paramOne:)", returning: "Hello World")
+        XCTAssertEqual(stubProvider.bar(paramOne: true), "Hello World")
+    }
+
+    func testDynamicallyStubbingFunctionWithSingleParameter() {
+        stubProvider.setDynamicStub(for: stubProvider.bar(paramOne:), withSignature: "bar(paramOne:)") { paramOne in
+            paramOne ? "Hello World" : "Goodbye World"
+        }
+
+        XCTAssertEqual(stubProvider.bar(paramOne: true), "Hello World")
+        XCTAssertEqual(stubProvider.bar(paramOne: false), "Goodbye World")
+    }
+
+    func testStubbingFunctionWithMultipleParameters() {
+        stubProvider.setStub(for: stubProvider.baz(paramOne:paramTwo:paramThree:), withSignature: "baz(paramOne:paramTwo:paramThree:)", returning: "Hello World")
+
+        XCTAssertEqual(stubProvider.baz(paramOne: true, paramTwo: "foo", paramThree: 12), "Hello World")
+    }
+
+    func testDynamicallyStubbingFunctionWithMultipleParameters() {
+        stubProvider.setDynamicStub(for: stubProvider.baz(paramOne:paramTwo:paramThree:), withSignature: "baz(paramOne:paramTwo:paramThree:)") { paramOne, paramTwo, paramThree in
+            paramOne ? paramTwo : "\(paramThree)"
+        }
+
+        XCTAssertEqual(stubProvider.baz(paramOne: true, paramTwo: "Hello World", paramThree: 7), "Hello World")
+        XCTAssertEqual(stubProvider.baz(paramOne: false, paramTwo: "Hello World", paramThree: 7), "7")
+    }
+
+    func testStubbingThrowingFunction_ReturningValue() {
+        stubProvider.setStub(for: stubProvider.oof, withSignature: "oof()", returning: 7)
+
+        XCTAssertEqual(try stubProvider.oof(), 7)
+    }
+
+    func testStubbingThrowingFunction_ThrowingError() {
+        stubProvider.setStub(for: stubProvider.oof, withSignature: "oof()", throwing: StubProviderError())
+
+        XCTAssertThrowsError(try stubProvider.oof()) { error in
+            XCTAssertTrue(error is StubProviderError)
+        }
+    }
+
+    func testDynamicallyStubbingThrowingFunction() {
+        var shouldThrow = false
+        stubProvider.setDynamicStub(for: stubProvider.oof, withSignature: "oof()") {
+            guard !shouldThrow else { throw StubProviderError() }
+            return 7
+        }
+
+        XCTAssertEqual(try stubProvider.oof(), 7)
+        shouldThrow = true
+        XCTAssertThrowsError(try stubProvider.oof()) { error in
+            XCTAssertTrue(error is StubProviderError)
+        }
+    }
+
+    func testStubbingGenericFunction() {
+        stubProvider.setStub(for: stubProvider.rab(paramOne:), withSignature: "rab(paramOne:)", returning: true)
+        stubProvider.setStub(for: stubProvider.rab(paramOne:), withSignature: "rab(paramOne:)", returning: 7)
+        stubProvider.setStub(for: stubProvider.rab(paramOne:), withSignature: "rab(paramOne:)", returning: "Hello World")
+
+        XCTAssertEqual(stubProvider.rab(paramOne: false), true)
+        XCTAssertEqual(stubProvider.rab(paramOne: 1), 7)
+        XCTAssertEqual(stubProvider.rab(paramOne: "Hello"), "Hello World")
+    }
+
+    func testDynamicallyStubbingGenericFunction() {
+        stubProvider.setDynamicStub(for: stubProvider.rab(paramOne:), withSignature: "rab(paramOne:)") { paramOne -> Bool in
+            !paramOne
+        }
+        stubProvider.setDynamicStub(for: stubProvider.rab(paramOne:), withSignature: "rab(paramOne:)") { paramOne -> Int in
+            paramOne * 7
+        }
+        stubProvider.setDynamicStub(for: stubProvider.rab(paramOne:), withSignature: "rab(paramOne:)") { paramOne -> String in
+            paramOne + " World"
+        }
+
+        XCTAssertEqual(stubProvider.rab(paramOne: false), true)
+        XCTAssertEqual(stubProvider.rab(paramOne: 1), 7)
+        XCTAssertEqual(stubProvider.rab(paramOne: "Hello"), "Hello World")
+    }
+
+    func testStubbingAsyncFunction() async {
+        stubProvider.setStub(for: stubProvider.zab, withSignature: "zab()", returning: 7)
+
+        let result = await stubProvider.zab()
+
+        XCTAssertEqual(result, 7)
+    }
+
+    func testDynamicallyStubbingAsyncFunction() async {
+        var x = 1
+        stubProvider.setDynamicStub(for: stubProvider.zab, withSignature: "zab()") {
+            x * 7
+        }
+
+        let resultOne = await stubProvider.zab()
+        x = 7
+        let resultTwo = await stubProvider.zab()
+
+        XCTAssertEqual(resultOne, 7)
+        XCTAssertEqual(resultTwo, 49)
+    }
+
+    func testStubbingAsyncThrowingFunction_ReturningValue() async throws {
+        stubProvider.setStub(for: stubProvider.zoo, withSignature: "zoo()", returning: 7)
+
+        let result = try await stubProvider.zoo()
+
+        XCTAssertEqual(result, 7)
+    }
+
+    func testStubbingAsyncThrowingFunction_ThrowingError() async throws {
+        stubProvider.setStub(for: stubProvider.zoo, withSignature: "zoo()", throwing: StubProviderError())
+
+        do {
+            _ = try await stubProvider.zoo()
+            XCTFail("Expected error to be thrown")
+        } catch _ as StubProviderError {
+            // Expected
+        } catch {
+            XCTFail("Expected error to be StubProviderError")
+        }
+    }
+
+    func testDynamicallyStubbingAsyncThrowingFunction_ReturningValue() async throws {
+        var x = 1
+        stubProvider.setDynamicStub(for: stubProvider.zoo, withSignature: "zoo()") {
+            x * 7
+        }
+
+        let resultOne = try await stubProvider.zoo()
+        x = 7
+        let resultTwo = try await stubProvider.zoo()
+
+        XCTAssertEqual(resultOne, 7)
+        XCTAssertEqual(resultTwo, 49)
+    }
+
+    func testDynamicallyStubbingAsyncThrowingFunction_ThrowingError() async throws {
+        var shouldThrow = false
+        stubProvider.setDynamicStub(for: stubProvider.zoo, withSignature: "zoo()") {
+            guard !shouldThrow else { throw StubProviderError() }
+            return 7
+        }
+
+        let result = try await stubProvider.zoo()
+        XCTAssertEqual(result, 7)
+
+        shouldThrow = true
+
+        do {
+            _ = try await stubProvider.zoo()
+            XCTFail("Expected error to be thrown")
+        } catch _ as StubProviderError {
+            // Expected
+        } catch {
+            XCTFail("Expected error to be StubProviderError")
+        }
+    }
+
+}
+
+private struct StubProvider: StubProviding {
+    var stubRegistry = StubRegistry()
+
+    func foo() -> Int {
+        stubOutput()
+    }
+
+    func bar(paramOne: Bool) -> String {
+        stubOutput(for: paramOne)
+    }
+
+    func baz(paramOne: Bool, paramTwo: String, paramThree: Int) -> String {
+        stubOutput(for: (paramOne, paramTwo, paramThree))
+    }
+
+    func oof() throws -> Int {
+        try throwingStubOutput()
+    }
+
+    func rab<T>(paramOne: T) -> T {
+        stubOutput(for: paramOne)
+    }
+
+    func zab() async -> Int {
+        stubOutput()
+    }
+
+    func zoo() async throws -> Int {
+        try throwingStubOutput()
+    }
+}
+
+private struct StubProviderError: Error {}
