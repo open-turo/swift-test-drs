d54ada6afb0381084b12bcce897484d20f5e9560
Shawn Gee <sgee@turo.com>
2024-05-22 15:04:08 -0700

    chore: add tests and update implementation

--- TestDRS/Sources/TestDRS/Stub/StubProviding+DefaultStorage.swift
+++ TestDRS/Sources/TestDRS/Stub/StubProviding+DefaultStorage.swift
@@ -7,8 +7,7 @@ import Foundation
 
 // These extensions provide default `StubRegistry` storage for types conforming to `StubProviding`.
 // Swift does not allow generic types to have stored static properties, so a separate storage mechanism is needed.
-// Extending `Hashable` and `Identifiable` types to be `StubProviding` is also enabled by providing a unique `StubRegistry`
-// for each instance of a type conforming to one of those protocols.
+// Extending `Identifiable` types to be `StubProviding` is also enabled by providing a unique `StubRegistry` for each identifiable instance.
 
 public extension StubProviding {
 
@@ -19,23 +18,12 @@ public extension StubProviding {
 
 }
 
-public extension StubProviding where Self: Hashable {
-
-    /// The default `BlackBox` for instances of types that are `Hashable`, allows for extending a hashable type to be a `Spy`.
-    var stubRegistry: StubRegistry {
-        StubRegistry.stubRegistry(
-            for: StorageKey(type: Self.self, hashable: self)
-        )
-    }
-
-}
-
 public extension StubProviding where Self: Identifiable {
 
     /// The default `BlackBox` for instances of types that are `Identifiable`, allows for extending an identifiable type to be a `Spy`.
     var stubRegistry: StubRegistry {
         StubRegistry.stubRegistry(
-            for: StorageKey(type: Self.self, hashable: id)
+            for: StorageKey(type: Self.self, id: id)
         )
     }
 
