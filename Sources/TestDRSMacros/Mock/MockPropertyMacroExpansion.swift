8f5bbd084369e33e232c04e70b0eca3338c9653f
Shawn Gee <sgee@turo.com>
2024-05-28 09:09:13 -0700

    feat: add diagnostics to MockProperty expansion

--- Sources/TestDRSMacros/Mock/MockPropertyMacroExpansion.swift
+++ Sources/TestDRSMacros/Mock/MockPropertyMacroExpansion.swift
@@ -10,6 +10,36 @@ import SwiftSyntaxMacros
 
 public struct MockPropertyMacro: AccessorMacro {
     public static func expansion(of node: AttributeSyntax, providingAccessorsOf declaration: some DeclSyntaxProtocol, in context: some MacroExpansionContext) throws -> [AccessorDeclSyntax] {
+        guard let variable = declaration.as(VariableDeclSyntax.self) else {
+            context.diagnose(
+                Diagnostic(
+                    node: declaration,
+                    message: MockPropertyExpansionDiagnostic.invalidType
+                )
+            )
+            return []
+        }
+
+        guard variable.bindingSpecifier.tokenKind == .keyword(.var) else {
+            context.diagnose(
+                Diagnostic(
+                    node: declaration,
+                    message: MockPropertyExpansionDiagnostic.immutable
+                )
+            )
+            return []
+        }
+
+        guard variable.bindings.allSatisfy({ $0.accessorBlock == nil }) else {
+            context.diagnose(
+                Diagnostic(
+                    node: declaration,
+                    message: MockPropertyExpansionDiagnostic.existingAccessor
+                )
+            )
+            return []
+        }
+
         return [
             AccessorDeclSyntax(accessorSpecifier: .keyword(.get)) {
                 "stubOutput()"
