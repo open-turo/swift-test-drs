dc4f0ecbea217b656787744d07e5e6b9c8efa029
Shawn Gee <sgee@turo.com>
2024-06-13 15:15:59 -0700

    refactor: add example client back as separate project

---
+++ ExampleClient/ExampleClient/ContentView.swift
@@ -0,0 +1,22 @@
+//
+// Created on 6/13/24.
+// Copyright © 2024 Turo Open Source. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
