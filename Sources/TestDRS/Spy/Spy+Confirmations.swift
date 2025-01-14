344511e50c948836bf0ad91a40e804c16e102176
Shawn Gee <sgee@turo.com>
2025-01-14 15:15:47 -0800

    chore: documentation

--- Sources/TestDRS/Spy/Spy+Confirmations.swift
+++ Sources/TestDRS/Spy/Spy+Confirmations.swift
@@ -5,8 +5,19 @@
 
 import Foundation
 
-extension Spy {
+public extension Spy {
 
+    /// Awaits confirmation of a function call.
+    ///
+    /// Similar to using a `Confirmation` in Swift Testing, this macro allows you to await confirmation that a function was called in
+    /// scenarios where you can't await the function directly.
+    ///
+    /// - Parameters:
+    ///   - function: A reference to the function to expect was called.
+    ///   - inputType: An optional phantom parameter used to derive the input type of the `function` passed in.
+    ///   - outputType: An optional phantom parameter used to derive the output type of the `function` passed in.
+    ///   - timeLimit: The maximum amount of time to wait for confirmation. If the time limit is reached before the first call can be confirmed, a test failure is reported. Defaults to a duration that is effectively infinite.
+    /// - Returns: A `FunctionCallConfirmation` that waits for the first matching call. Further calls can be confirmed by calling methods on this confirmation.
     @discardableResult
     func confirmationOfCall<Input, Output>(
         to function: (Input) async throws -> Output,
@@ -35,6 +46,17 @@ extension Spy {
         )
     }
 
+    /// Awaits confirmation of a function call with an expected input.
+    ///
+    /// Similar to using a `Confirmation` in Swift Testing, this macro allows you to await confirmation that a function was called in
+    /// scenarios where you can't await the function directly.
+    ///
+    /// - Parameters:
+    ///   - function: A reference to the function to expect was called.
+    ///   - expectedInput: The expected input parameter(s) for the function.
+    ///   - outputType: An optional phantom parameter used to derive the output type of the `function` passed in.
+    ///   - timeLimit: The maximum amount of time to wait for confirmation. If the time limit is reached before the first call can be confirmed, a test failure is reported. Defaults to a duration that is effectively infinite.
+    /// - Returns: A `FunctionCallConfirmation` that waits for the first matching call. Further calls can be confirmed by calling methods on this confirmation.
     @discardableResult
     func confirmationOfCall<each Input: Equatable, Output>(
         to function: (repeat each Input) async throws -> Output,
@@ -47,7 +69,7 @@ extension Spy {
         filePath: StaticString = #filePath,
         line: UInt = #line,
         column: UInt = #column
-    ) async -> FunctionCallConfirmation < MatchingFirst, (repeat each Input), Output> {
+    ) async -> FunctionCallConfirmation <MatchingFirst, (repeat each Input), Output> {
         let location = SourceLocation(fileID: fileID, filePath: filePath, line: line, column: column)
         let stream = blackBox.streamForCallsMatching(
             signature: signature,
