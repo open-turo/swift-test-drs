fa3ea14434b629970300bdf949e16c20d819c8db
Shawn Gee <sgee@renewedvision.com>
2025-06-08 21:55:27 -0700

    feat: expect case first pass

---
+++ Sources/TestDRSMacros/Utility/ExpectCaseMacroExpansion.swift
@@ -0,0 +1,181 @@
+//
+// Created on 6/6/25.
+// Copyright © 2025 Turo Open Source. All rights reserved.
+//
+
+import SwiftCompilerPlugin
+import SwiftDiagnostics
+import SwiftSyntax
+import SwiftSyntaxBuilder
+import SwiftSyntaxMacros
+
+enum MacroExpansionError: Error, CustomStringConvertible {
+    case missingArguments
+
+    var description: String {
+        switch self {
+        case .missingArguments:
+            return "Missing required arguments"
+        }
+    }
+}
+
+public struct ExpectCaseMacro: ExpressionMacro {
+    public static func expansion(
+        of node: some FreestandingMacroExpansionSyntax,
+        in context: some MacroExpansionContext
+    ) throws -> ExprSyntax {
+        guard let caseArgument = node.arguments.first else {
+            throw MacroExpansionError.missingArguments
+        }
+
+        guard let valueArgument = node.arguments.dropFirst().first else {
+            throw MacroExpansionError.missingArguments
+        }
+
+        let casePattern = caseArgument.expression.trimmed
+        let valueExpression = valueArgument.expression.trimmed
+
+        guard let caseName = extractCaseName(from: casePattern) else {
+            context.diagnose(
+                Diagnostic(
+                    node: Syntax(caseArgument),
+                    message: ExpectCaseExpansionDiagnostic.invalidCasePattern
+                )
+            )
+            return ExprSyntax(stringLiteral: "")
+        }
+
+        let switchStatement = buildSwitchStatement(
+            valueExpression: valueExpression,
+            casePattern: casePattern,
+            caseName: caseName
+        )
+
+        return ExprSyntax(
+            FunctionCallExprSyntax(
+                calledExpression: ExprSyntax(
+                    ClosureExprSyntax(
+                        leftBrace: .leftBraceToken(),
+                        statements: CodeBlockItemListSyntax([
+                            CodeBlockItemSyntax(item: .expr(ExprSyntax(switchStatement)))
+                        ]),
+                        rightBrace: .rightBraceToken()
+                    )
+                ),
+                leftParen: .leftParenToken(),
+                arguments: LabeledExprListSyntax([]),
+                rightParen: .rightParenToken()
+            )
+        )
+    }
+
+    private static func extractCaseName(from expression: ExprSyntax) -> String? {
+        if let memberAccess = expression.as(MemberAccessExprSyntax.self) {
+            return memberAccess.declName.baseName.text
+        }
+
+        if let functionCall = expression.as(FunctionCallExprSyntax.self),
+           let memberAccess = functionCall.calledExpression.as(MemberAccessExprSyntax.self) {
+            return memberAccess.declName.baseName.text
+        }
+
+        return nil
+    }
+
+    private static func buildSwitchStatement(
+        valueExpression: ExprSyntax,
+        casePattern: ExprSyntax,
+        caseName: String
+    ) -> SwitchExprSyntax {
+        let expectedCase = SwitchCaseSyntax(
+            label: .case(
+                SwitchCaseLabelSyntax(
+                    caseItems: SwitchCaseItemListSyntax([
+                        SwitchCaseItemSyntax(
+                            pattern: ExpressionPatternSyntax(expression: casePattern)
+                        )
+                    ])
+                )
+            ),
+            statements: CodeBlockItemListSyntax([
+                CodeBlockItemSyntax(
+                    item: .stmt(StmtSyntax(BreakStmtSyntax()))
+                )
+            ])
+        )
+
+        let errorMessage = buildErrorMessage(
+            caseName: caseName,
+            valueExpression: valueExpression,
+            casePattern: casePattern
+        )
+        let defaultCase = SwitchCaseSyntax(
+            label: .default(SwitchDefaultLabelSyntax()),
+            statements: CodeBlockItemListSyntax([
+                CodeBlockItemSyntax(
+                    item: .stmt(
+                        StmtSyntax(
+                            ExpressionStmtSyntax(
+                                expression: FunctionCallExprSyntax(
+                                    calledExpression: ExprSyntax(DeclReferenceExprSyntax(baseName: .identifier("reportFailure"))),
+                                    leftParen: .leftParenToken(),
+                                    arguments: LabeledExprListSyntax([
+                                        LabeledExprSyntax(expression: errorMessage)
+                                    ]),
+                                    rightParen: .rightParenToken()
+                                )
+                            )
+                        )
+                    )
+                )
+            ])
+        )
+
+        return SwitchExprSyntax(
+            subject: valueExpression,
+            leftBrace: .leftBraceToken(),
+            cases: SwitchCaseListSyntax([
+                SwitchCaseListSyntax.Element(expectedCase),
+                SwitchCaseListSyntax.Element(defaultCase)
+            ]),
+            rightBrace: .rightBraceToken()
+        )
+    }
+
+    private static func buildErrorMessage(caseName: String, valueExpression: ExprSyntax, casePattern: ExprSyntax) -> ExprSyntax {
+        let expectedPart = StringLiteralExprSyntax(content: "Expected ")
+        let casePatternString = StringLiteralExprSyntax(content: casePattern.description)
+        let middlePart = StringLiteralExprSyntax(content: ", but got ")
+
+        let stringDescribingCall = FunctionCallExprSyntax(
+            calledExpression: ExprSyntax(DeclReferenceExprSyntax(baseName: .identifier("String"))),
+            leftParen: .leftParenToken(),
+            arguments: LabeledExprListSyntax([
+                LabeledExprSyntax(label: "describing", colon: .colonToken(), expression: valueExpression)
+            ]),
+            rightParen: .rightParenToken()
+        )
+
+        // Concatenate: "Expected " + pattern + ", but got " + description
+        let firstConcat = InfixOperatorExprSyntax(
+            leftOperand: ExprSyntax(expectedPart),
+            operator: BinaryOperatorExprSyntax(operator: .binaryOperator("+")),
+            rightOperand: ExprSyntax(casePatternString)
+        )
+
+        let secondConcat = InfixOperatorExprSyntax(
+            leftOperand: ExprSyntax(firstConcat),
+            operator: BinaryOperatorExprSyntax(operator: .binaryOperator("+")),
+            rightOperand: ExprSyntax(middlePart)
+        )
+
+        let finalConcat = InfixOperatorExprSyntax(
+            leftOperand: ExprSyntax(secondConcat),
+            operator: BinaryOperatorExprSyntax(operator: .binaryOperator("+")),
+            rightOperand: ExprSyntax(stringDescribingCall)
+        )
+
+        return ExprSyntax(finalConcat)
+    }
+}
